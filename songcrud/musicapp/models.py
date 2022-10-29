d0208adb4c177a20e4387c066b95ed4560ec000f
victorpatricks@yahoo.com <101418154+VakaHub@users.noreply.github.com>
2022-10-29 03:26:33 +0100

    Week 5 Python Task: Django CRUD Commit
    - Create/define models in models.py
    
    - Register models in admin.py
    
    - Test/Run Server
    
    - Make Migrations
    
    - Migrate
    
    - Create Super-User
    
    - Run Server and Login to Admin

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.first_name
+
+class Song(models.Model):
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    release_date = models.DateField()
+    like = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.content
